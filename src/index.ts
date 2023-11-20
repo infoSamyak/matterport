9705cf4881ce1e5d846781743884b0c237bf948d
Samyak <samyak.agrawal@infobeans.com>
2023-11-20 21:13:59 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-const showcase = document.getElementById('showcase') as HTMLIFrameElement;
-const key = 'YOUR_KEY_HERE';
+const showcase = document.getElementById("showcase") as HTMLIFrameElement;
+const key = "n6npxqct88mnqea6b9m7w4cea";
 
 // declare this file is a module
 export {};
 
 // augment window with the MP_SDK property
 declare global {
-  interface Window {
-    MP_SDK: any;
-  }
+	interface Window {
+		MP_SDK: any;
+	}
 }
 
-showcase.addEventListener('load', async function() {
-  let sdk;
-  try {
-    sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, '3.6');
-  }
-  catch(e) {
-    console.error(e);
-    return;
-  }
+showcase.addEventListener("load", async function () {
+	console.log("%c  Hello Bundle SDK! ", "background: #333333; color: #00dd00");
 
-  console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
-  console.log(sdk);
+	let sdk;
+	try {
+		sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key, "3.6");
+	} catch (e) {
+		console.error(e);
+		return;
+	}
+
+	console.log("%c  Hello Bundle SDK! ", "background: #333333; color: #00dd00");
+	console.log(sdk);
 });
